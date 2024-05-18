a246c5b598a5e1c3b29ea595739cfa5757d228b5
Alisson Dutra <alisson.s.dutra@gmail.com>
2024-05-18 20:24:07 -0300

    Intial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'; 
+import ListaDeTarefas from './ListaDeTarefas'; 
+
+  
+
+function App() { 
+
+  return ( 
+
+    <div className="App"> 
+
+      <ListaDeTarefas /> 
+
+    </div> 
+
+  ); 
+
+} 
+
+  
+
+export default App; 
